aaba0ddee7849d7688ef945ee50594c78317291d
Sardor Allabergenov <Sardor.Allabergenov@universalbank.uz>
2020-10-12 09:34:45 +0500

    last

--- data/js/index.js
+++ data/js/index.js
@@ -78,15 +78,14 @@ $(document).ready(function () {
     });
 });
 
-
 $(document).ready(function () {
     $(".thumbnails img").click(function () {
         $(this).addClass("active").siblings().removeClass("active");
         $(".mainImg img").hide().attr("src", $(this).attr("src")).fadeIn(300);
     });
 
-    $(".mainImg img").click(function (event){
-        event.stopPropagation()
+    $(".mainImg img").click(function (event) {
+        event.stopPropagation();
         $(".modal-img").attr("src", $(this).attr("src"));
         $(".image-modal").fadeIn(300);
     });
@@ -94,4 +93,94 @@ $(document).ready(function () {
     $(window).on("click", function () {
         $(".image-modal").fadeOut(300);
     });
-});    
+});
+
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//         var position = $(window).scrollTop();
+//         var bottom = $(document).height() - $(window).height();
+
+//         if (position == bottom) {
+//             var row = Number($("#row").val());
+//             var allcount = Number($("#all").val());
+//             var rowperpage = 3;
+//             row = row + rowperpage;
+
+//             if (row <= allcount) {
+//                 $("#row").val(row);
+//                 var page = 1;
+//                 $.ajax({
+//                     url:
+//                         "https://api.themoviedb.org/3/movie/upcoming?api_key=a4942ba2bb35ba2f7205bedb5a8989bc&page=" +
+//                         page,
+//                     type: "get",
+//                     data: { row: row },
+//                     success: function (response) {
+//                         for (var i = 0; i < response.results.length; i++) {
+//                             var title = response.results[i].title;
+//                             console.log(i + 1, response.results[i].title);
+//                             $("#postTitle:last")
+//                                 .after(`<li>${title}</li>`)
+//                                 .show()
+//                                 .fadeIn("slow");
+//                             page++;
+//                         }
+//                     },
+//                 });
+//             }
+//         }
+//     });
+// });
+
+$(document).ready(function () {
+    var pageIs = 1;
+
+    if($('#load_text_temp').html() === ''){
+        $.ajax({
+            url:
+                "https://api.themoviedb.org/3/movie/upcoming?api_key=a4942ba2bb35ba2f7205bedb5a8989bc&page=" +
+                1,
+            type: "GET",
+            // data: JSON.stringify({ page: page }),
+            // contentType: "application/json; charset=UTF-8",
+            dataType: "json",
+            success: function (response) {
+                for (var i = 0; i < response.results.length; i++) {
+                    var title = response.results[i].title;
+                    console.log("page ", pageIs);
+                    $(`<li>${title}</li>`).hide()
+                        .appendTo($('#load_text_temp'))
+                        .show().fadeIn('slow');
+                }
+                pageIs++;
+                console.log("page ", pageIs);
+            },
+        });
+    }
+    $(window).scroll(function () {
+        var position = $(window).scrollTop();
+        var bottom = $(document).height() - $(window).height();
+        
+        if (position == bottom) {
+            $.ajax({
+                url:
+                    "https://api.themoviedb.org/3/movie/upcoming?api_key=a4942ba2bb35ba2f7205bedb5a8989bc&page=" +
+                    pageIs,
+                type: "GET",
+                contentType: "application/json; charset=UTF-8",
+                dataType: "json",
+                success: function (response) {
+                    for (var i = 0; i < response.results.length; i++) {
+                        // var title = response.results[i].title;
+                        // console.log("page ", pageIs);
+                        $(`<li>${response.results[i].title}</li>`).hide().appendTo($('#load_text_temp')).fadeIn(300);
+                    }
+                    pageIs++;
+                    console.log("page ", pageIs);
+                },
+            });
+            return false;
+        }
+    });
+
+});
