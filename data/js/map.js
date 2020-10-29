ab59b40f090da1679f5503ba45343a87dabbfa63
Sardor Allabergenov <Sardor.Allabergenov@universalbank.uz>
2020-10-29 10:03:07 +0500

    last

--- data/js/map.js
+++ data/js/map.js
@@ -76,7 +76,7 @@ function showPosition(position) {
                         },
                     };
                 }
-                var lines = {
+                var layers = {
                     type: "geojson",
                     data: {
                         type: "FeatureCollection",
@@ -84,7 +84,7 @@ function showPosition(position) {
                     },
                 };
 
-                map.addSource("points", lines);
+                map.addSource("points", layers);
 
                 map.addLayer({
                     id: "points",
@@ -102,61 +102,8 @@ function showPosition(position) {
                         "text-anchor": "top",
                     },
                 });
-
+                // map.scrollZoom.disable();
+                });
             });
-        map.on("load", function () {
-            // Insert the layer beneath any symbol layer.
-            var layers = map.getStyle().layers;
-
-            var labelLayerId;
-            for (var i = 0; i < layers.length; i++) {
-                if (
-                    layers[i].type === "symbol" &&
-                    layers[i].layout["text-field"]
-                ) {
-                    labelLayerId = layers[i].id;
-                    break;
-                }
-            }
-
-            map.addLayer(
-                {
-                    id: "3d-buildings",
-                    source: "composite",
-                    "source-layer": "building",
-                    filter: ["==", "extrude", "true"],
-                    type: "fill-extrusion",
-                    minzoom: 15,
-                    paint: {
-                        "fill-extrusion-color": "#aaa",
-
-                        // use an 'interpolate' expression to add a smooth transition effect to the
-                        // buildings as the user zooms in
-                        "fill-extrusion-height": [
-                            "interpolate",
-                            ["linear"],
-                            ["zoom"],
-                            15,
-                            0,
-                            15.05,
-                            ["get", "height"],
-                        ],
-                        "fill-extrusion-base": [
-                            "interpolate",
-                            ["linear"],
-                            ["zoom"],
-                            15,
-                            0,
-                            15.05,
-                            ["get", "min_height"],
-                        ],
-                        "fill-extrusion-opacity": 0.6,
-                    },
-                },
-                labelLayerId
-            );
-        });
-            // map.scrollZoom.disable();
-        });
     });
 }
